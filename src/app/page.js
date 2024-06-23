dd9835af870068cbecd2092fbfb78a883dba5cfd
Arbab Raheem <121390455+Arbab02@users.noreply.github.com>
2024-06-23 03:04:37 -0700

    Image sharer is commented

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,12 @@
 import TextSharer from './TextSharer/page'
-import ImageSharer from './Storage/page'
+//import ImageSharer from './Storage/page'
 import Footer from './components/Footer'
 
 const App = () => {
   return (
     <>
     <TextSharer/>
-    <ImageSharer/>
+ //   <ImageSharer/>
     <Footer/>
     
     
